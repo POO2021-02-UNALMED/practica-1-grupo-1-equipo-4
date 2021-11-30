bbb983996c75b301e1bc5bfbad116b63a2855773
ddaza030 <danidaza08@gmail.com>
2021-11-30 18:36:37 -0500

    creacion de silla y sala

--- src/gestorAplicacion/salas/Sala.java
+++ src/gestorAplicacion/salas/Sala.java
@@ -1,5 +1,100 @@
 package gestorAplicacion.salas;
 
-public class Sala {
+import java.util.ArrayList;
+import gestorAplicacion.boleteria.Funcion;
 
+public class Sala {
+	private int numero;
+	private int filas;
+	private int columnas;
+	private int filasvip;
+	private float precio;
+	private ArrayList<Silla> sillas = new ArrayList<Silla>();
+	private ArrayList<Funcion> funciones = new ArrayList<Funcion>();
+	
+	public Sala(int numero, int filas, int columnas, int filasvip, int precio) {
+		this.numero = numero;
+		this.filas = filas;
+		this.columnas = columnas;
+		this.filasvip = filasvip;
+	}
+
+	
+	
+	public void crearSilleteria() {
+		
+		int total = filas*columnas; 		//número de filas
+		int totalvip = filasvip*columnas;	//número de filas vip
+		String tipo = "VIP";
+		
+		for(int i = 0;i<total;i++) {
+			
+			if(totalvip<=0) {
+				tipo = "SENCILLA";
+			}
+			else {
+				totalvip--;
+			}
+			
+			Silla silla = new Silla(tipo,i+1);
+			sillas.add(silla);
+		}
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getFilas() {
+		return filas;
+	}
+
+	public void setFilas(int filas) {
+		this.filas = filas;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public int getColumnas() {
+		return columnas;
+	}
+
+	public void setColumnas(int columnas) {
+		this.columnas = columnas;
+	}
+
+	public ArrayList<Silla> getSillas() {
+		return sillas;
+	}
+
+	public void setSillas(ArrayList<Silla> sillas) {
+		this.sillas = sillas;
+	}
+
+	public ArrayList<Funcion> getFunciones() {
+		return funciones;
+	}
+
+	public void setFunciones(ArrayList<Funcion> funciones) {
+		this.funciones = funciones;
+	}
+
+	public int getFilasVip() {
+		return filasvip;
+	}
+
+	public void setFilasVip(int vip) {
+		this.filasvip = vip;
+	}
+	
 }
--- src/gestorAplicacion/salas/Silla.java
+++ src/gestorAplicacion/salas/Silla.java
@@ -1,5 +1,40 @@
 package gestorAplicacion.salas;
 
 public class Silla {
-
+	private enum Tipo {VIP, SENCILLA};
+	private Tipo tipo;
+	private int numero;
+	private int precio;
+	
+	
+	public Silla(String tipo, int numero) {
+		setTipo(tipo); // se debe establecer el precio depende del tipo
+		setNumero(numero);
+	}
+	
+	
+	
+	public Tipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		if(tipo.equals("VIP")) {
+			this.tipo = Tipo.VIP;
+		}else {
+			this.tipo = Tipo.SENCILLA;
+		}
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
 }
