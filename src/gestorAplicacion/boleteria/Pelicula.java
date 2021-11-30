920735cb77f1f174f8988fa9d1953e32e01aa5f1
Marlondot <80053997+Marlondot@users.noreply.github.com>
2021-11-30 18:08:24 -0500

    Pelicula Cine y Cliente, y sus fields
    Se agregaron las clases Pelicula, Cliente, y Cine, además de sus atributos.

--- src/gestorAplicacion/boleteria/Pelicula.java
+++ src/gestorAplicacion/boleteria/Pelicula.java
@@ -1,5 +1,58 @@
 package gestorAplicacion.boleteria;
 
 public class Pelicula {
+	private String nombre;
+	private int duracion;
+	private String lenguaje;
+	private int clasificacion;
+	private int cantidadTotalBoletosVendidos;
+	private int expectativaVentas;
+	
+	
+	//
+	//getting and setting:
+	//
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	
+	public String getLenguaje() {
+		return lenguaje;
+	}
+	public void setLenguaje(String lenguaje) {
+		this.lenguaje = lenguaje;
+	}
+	
+	public int getClasificacion() {
+		return clasificacion;
+	}
+	public void setClasificacion(int clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+	
+	public int getCantidadTotalBoletosVendidos() {
+		return cantidadTotalBoletosVendidos;
+	}
+	public void setCantidadTotalBoletosVendidos(int cantidadTotalBoletosVendidos) {
+		this.cantidadTotalBoletosVendidos = cantidadTotalBoletosVendidos;
+	}
+	
+	public int getExpectativaVentas() {
+		return expectativaVentas;
+	}
+	public void setExpectativaVentas(int expectativaVentas) {
+		this.expectativaVentas = expectativaVentas;
+	}
 
 }
