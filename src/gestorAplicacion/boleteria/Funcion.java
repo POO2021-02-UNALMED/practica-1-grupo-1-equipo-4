1e1afd56c92ec9410e75b686d96d215481ee183f
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2021-12-04 23:06:36 -0500

    Atributos
    Se incluyeron atributos con sus get y set

--- src/gestorAplicacion/boleteria/Funcion.java
+++ src/gestorAplicacion/boleteria/Funcion.java
@@ -1,7 +1,64 @@
 package gestorAplicacion.boleteria;
 
 import java.io.Serializable;
+import java.util.*;
 
 public class Funcion implements Serializable{
 	private static final long serialVersionUID = 2L;
+	
+	private Date fecha;
+	private String horario;
+	private int duracion;
+	private Pelicula pelicula;
+	private List<String> lenguajes = new ArrayList<String>();
+	private int cantidadBoletosVendidos;
+	
+	//
+	//getting and setting
+	//
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+	public List<String> getLenguajes() {
+		return lenguajes;
+	}
+	public void setLenguajes(List<String> lenguajes) {
+		this.lenguajes = lenguajes;
+	}
+	public int getCantidadBoletosVendidos() {
+		return cantidadBoletosVendidos;
+	}
+	public void setCantidadBoletosVendidos(int cantidadBoletosVendidos) {
+		this.cantidadBoletosVendidos = cantidadBoletosVendidos;
+	}
+	
+	
+	//
+	//methods
+	//
 }
