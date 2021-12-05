4ef9522b03a7d10f1ff32f3bf3c668f7ee291bfc
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2021-12-04 22:33:07 -0500

    Atributos
    Con getter y setter

--- src/gestorAplicacion/boleteria/Boleto.java
+++ src/gestorAplicacion/boleteria/Boleto.java
@@ -1,5 +1,31 @@
 package gestorAplicacion.boleteria;
 
 public class Boleto {
+	private long id;
+	private float precioTotal;
+	private boolean disponibilidad;
 
+	//
+	// getting and setting:
+	//
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	public boolean isDisponibilidad() {
+		return disponibilidad;
+	}
+	public void setDisponibilidad(boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
 }
