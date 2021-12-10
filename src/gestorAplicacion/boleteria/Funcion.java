4a8e087feed9be058383ca818f121b8fe40a930c
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-12-10 14:04:50 -0500

    Funcion sin sabotaje de daza

--- src/gestorAplicacion/boleteria/Funcion.java
+++ src/gestorAplicacion/boleteria/Funcion.java
@@ -195,8 +195,9 @@ public class Funcion implements Serializable{
 	}
 	
 	// MAIN PARA ENSAYAR
-	
-	public static void main(String[] args){
+
+	/*
+	 	public static void main(String[] args){
 		Pelicula rey_leon = new Pelicula();
 		Pelicula avenger = new Pelicula();
 		rey_leon.setNombre("El Rey Leon");
@@ -205,11 +206,12 @@ public class Funcion implements Serializable{
 		Funcion funcion_1 = new Funcion(1, 3, Horario.UNO, rey_leon, sala1);
 		funcion_1.getBoletos().get(3).setDisponibilidad(false);
 		System.out.println(funcion_1.verDisponibilidad());
-		/*Funcion funcion_2 = new Funcion(1,3,Horario.DOS, avenger, sala1);
+		Funcion funcion_2 = new Funcion(1,3,Horario.DOS, avenger, sala1);
 		ArrayList<Funcion> funciones = new ArrayList<Funcion>();
 		funciones.add(funcion_1);
 		funciones.add(funcion_2);
-		System.out.println(Cine.formatearFunciones(funciones));*/
-	}
+		System.out.println(Cine.formatearFunciones(funciones));
+	} 
+	*/
 	
 }
