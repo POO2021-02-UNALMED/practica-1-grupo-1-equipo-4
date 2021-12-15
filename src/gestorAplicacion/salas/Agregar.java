37f9163b4b7972d663d8db683101198a7bcb7594
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-12-15 18:55:26 -0500

    solucion problema con interfaz

--- src/gestorAplicacion/salas/Agregar.java
+++ src/gestorAplicacion/salas/Agregar.java
@@ -1,8 +1,10 @@
 package gestorAplicacion.salas;
 
+import gestorAplicacion.boleteria.Funcion;
+
 public interface Agregar {
 	
-	void agregarFuncion();
+	void agregarFuncion(Funcion funcion);
 	//se crea una interfaz sencilla con el metodo agregar funcion
 	//para que la clase Sala lo implemente
 }
