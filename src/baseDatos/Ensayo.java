bd6f121ae020700203fa5b09d62b20064f7b13c2
Daniel Daza <80338410+ddaza030@users.noreply.github.com>
2021-12-16 08:07:41 -0500

    stash

--- src/baseDatos/Ensayo.java
+++ src/baseDatos/Ensayo.java
@@ -9,7 +9,7 @@ public class Ensayo {
 	public static void main(String[] args){
 
         // pelis 
-		/*
+		
 		Pelicula rey_leon = new Pelicula();
 		Pelicula avenger = new Pelicula();
 		rey_leon.setNombre("El Rey Leon");
@@ -17,7 +17,7 @@ public class Ensayo {
 
 
         // cine 
-        Cine vizcaya = new Cine("Vizcaya");
+        Cine vizcaya = new Cine("Bahia");
 		
 		Sala2D sala1 = new Sala2D(1, 7, 8, 2);
         Sala3D sala2 = new Sala3D(2, 9, 9, 2, 36);
@@ -29,8 +29,8 @@ public class Ensayo {
 		//funcion_1.getBoletos().get(3).setDisponibilidad(false);
 		//System.out.println(funcion_1.verDisponibilidad());
 		Funcion funcion_2 = Funcion.crearFuncion(1,3,Horario.DOS, avenger, 2, vizcaya);
-
-		Serializador.serializar(vizcaya);*/
+		
+		System.out.println(funcion_1.verDisponibilidad());
 		/*Cine cine = Deserializador.deserializar();
 		
 		System.out.println(cine.getPeliculas().get(0).verDisponibilidad());
