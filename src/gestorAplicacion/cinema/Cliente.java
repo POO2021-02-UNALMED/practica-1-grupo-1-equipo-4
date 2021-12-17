1222dc2e4485710b92b63316e1eb10ea37cd334b
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-12-16 20:49:30 -0500

    cliente comentado

--- src/gestorAplicacion/cinema/Cliente.java
+++ src/gestorAplicacion/cinema/Cliente.java
@@ -1,16 +1,26 @@
+/*
+Funcionalidad: Almacenar toda la informacion respectiva a los clientes asi como regular el descuento que se le hace
+añadir a sus referidos y determinar su genero preferido
+ */
+
+//Autores: Daniel Santiago Cadavid, Marlon Calle, Daniel Daza, Juan Esteban Ochoa
+
 package gestorAplicacion.cinema;
 import java.io.Serializable;
 import java.util.*;
 import gestorAplicacion.boleteria.*;
 
-public class Cliente implements Serializable{ //??? should this implements Serializable
+public class Cliente implements Serializable{ 
+	/*
+	Para establecer todo lo referido a los datos del cliente, sus descuentos 
+	 */
 	private static final long serialVersionUID = 1L;
 	private int cedula;
 	private String nombre;
-	private int edad; //??? Es necesaria una edad para el cliente
+	private int edad;
 	private String ocupacion;
 	private float descuento;
-	public List<Boleto> historialCompras= new ArrayList<Boleto>();//??? Why is this public?
+	public List<Boleto> historialCompras= new ArrayList<Boleto>(); //una lista con los boletos que ha comprado el cliente en el cine a traves de su vida
 	private int referidos;
 	private Cine cine;
 	
@@ -36,9 +46,12 @@ public class Cliente implements Serializable{ //??? should this implements Seria
 	
 	//Aplicacion de descuentos
 	public String descuentoCliente() {
-		cine.mostValueClient();
+		/*
+		Este metodo no recibe nada, pero devuelve un String informando que se le realizo el descuento al cliente mas valioso o al que tenga referidos
+		 */
+		cine.mostValueClient();		//se le otorga un descuento del 40% al cliente que mas ha comprado
 		
-		if(descuento<=(float)0.39 && referidos>0) {      					//Si los descuentos no sobrepasan el 0.4, se aplican
+		if(descuento<=(float)0.39 && referidos>0) {      		//Si los descuentos no sobrepasan el 0.4, se aplican
 			this.descuento+=(float) (0.01*referidos);
 		}
 		
@@ -48,30 +61,39 @@ public class Cliente implements Serializable{ //??? should this implements Seria
 	
 	//Reflejar la pelicula más vista
 	public String mostWatchedGenre() {
-		List<String> genreList=new ArrayList<String>();
+		/*
+		No recibe nada pero devuelve una string con el genero mas visto del cliente
+		 */
+		List<String> genreList=new ArrayList<String>();		//lista para tener los generos que ha visto el cliente
 		for(Boleto boleto: historialCompras) {
 			genreList.add(boleto.getFuncion().getPelicula().getGenero()); 	//Recorre el historial de compras del cliente y anexa de los boletos sus generos
 		}
-		List<Integer> cuenta=new ArrayList<Integer>();
+		List<Integer> cuenta=new ArrayList<Integer>();		//lista para guardar la frecuencia de cada genero
 		for(String genre: genreList) {
 			int occ = Collections.frequency(genreList, genre);	//De la lista de géneros extrae la frecuencia de cada elemento
 			cuenta.add(occ);
 		}
 		
-		return genreList.get(cuenta.indexOf(Collections.max(cuenta))); 
+		return genreList.get(cuenta.indexOf(Collections.max(cuenta))); 	//se devuelve el genero de la lista que tenga el indice correspondiente al maximo de las ocurrencias de cada genero
 		
 	}
 
 	
-	//Sumar referidos al cliente
+	
 	public void referidos() {
+		/*
+		no recibe ni devuelve nada, su proposito es sumar referidos al cliente y llamar el metodo para aplicar el descuento por cada referido
+		 */
 		this.referidos++;
 		this.descuentoCliente();
 	}
 	
-	// Method toString en cliente para facilitar el print de algunos de sus atributos en el UI.
+	
 	@Override
-	public String toString() { 
+	public String toString() {
+		/*
+		toString en cliente para facilitar el print de algunos de sus atributos en el UI.
+		 */
 		return "Cliente: " + nombre + "-" + String.valueOf(edad) + "-" + ocupacion;
 		
 	}
