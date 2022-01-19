8bd4b5f2bbd8d595eba7abd15c2326349ef58a6a
Daniel Daza <80338410+ddaza030@users.noreply.github.com>
2022-01-19 17:03:23 -0500

    Create sala3D.py

---
+++ src/gestionAplicacion/salas/sala3D.py
@@ -0,0 +1,9 @@
+from ..cinemas import Cine
+class sala3D:
+
+    def __init__(self, *args):
+        if(len(args) == 4):
+            super().__init__()
+		super(filas, columnas, 0, 5000,cine);
+		this.cantidadGafas=cantidadgafas;
+	}
