7d9bc96457e7559279e8fe5075515ee31823e4a5
Daniel Daza <80338410+ddaza030@users.noreply.github.com>
2022-01-19 18:16:09 -0500

    Update sala2D.py

--- src/gestionAplicacion/salas/sala2D.py
+++ src/gestionAplicacion/salas/sala2D.py
@@ -1,12 +1,35 @@
+from salas import Sala, Silla
 
 class Sala2D(Sala):
 
 
-    public Sala2D(int filas, int columnas, int filasvip,Cine cine) {
-		super(filas, columnas, filasvip, 2000,cine);
-	}
-	
-	//Sala 2D para un tamano de sala default
-	public Sala2D(int vip, Cine cine) {
-		this(8, 12, vip, cine);
-	}
+    def __init__ (self,*args):
+        if(len(args) == 4):
+            filas, columnas, filasvip, cine = args
+            super().__init__(filas, columnas, filasvip, 2000,cine);
+            
+        elif(len(args) == 2):
+            vip , cine = args
+            super().__init__(8, 12, vip, 2000, cine);
+
+    
+    def cantidadSillas(self):
+        return len(super().getSillas())
+    
+    def crearSilleteria(self):
+        total: int =  self._filas*self._columnas
+        totalvip: int = self._filasvip*self._columnas
+
+        tipo : str = "VIP"
+
+        for i in range(total):
+            
+            if(totalvip<=0):				
+                tipo = "SENCILLA"
+
+            else:								
+                totalvip-=1
+                
+            silla : Silla = Silla(tipo,i+1)
+            
+            self._sillas.append(silla)
