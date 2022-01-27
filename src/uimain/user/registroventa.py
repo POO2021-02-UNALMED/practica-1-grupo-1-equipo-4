f28b6943cd5aac1e18827af48be14977950a41c1
Marlondot <mcallea@unal.edu.co>
2022-01-27 18:45:23 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/uimain/user/registroventa.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+class RegistroVenta(Frame):
+    
+    def __init__(self,window):
+        super.__init__(window)
+        self.verificacion()
+
+    def vervicacion():
+
+
+window=Tk
+RegistroVenta(window)
