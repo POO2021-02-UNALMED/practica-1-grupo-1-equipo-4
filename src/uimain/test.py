c8673e30060d6fe459f245e1d92f2eb55cd2a09e
Marlondot <mcallea@unal.edu.co>
2022-01-27 10:48:39 -0500

    Ayuda

--- src/uimain/test.py
+++ src/uimain/test.py
@@ -1,11 +1,34 @@
-import tkinter as tk
-
-
-window = tk.Tk
-
-
-frame_1=tk.Frame(height=50)
-
-frame_1.pack()
-
-print("Not funny")
+from cProfile import label
+from tkinter import *
+from tkinter import messagebox
+window = Tk()
+window.geometry("300x300")
+window.option_add('*tearOff', FALSE)
+framea = Frame(window, width=100, height=100, bg="red")
+label=Label(framea,text="Ahhh")
+frameb = Frame(window, width=100, height=100, bg="blue")
+text=Text(framea,height=5, width=13)
+def message():
+    INPUT = text.get("1.0", "end-1c")
+    messagebox.showinfo(
+        title="Test",
+        message=INPUT
+    )
+def frame_a(e):
+    if e=="<Enter>":
+        label["text"]="Adentro"
+    else:
+        label["text"]="Afuera"
+    print(e)
+    print("Posición x:", e.x, "y:", e.y)
+window.bind('<Enter>', frame_a)
+window.bind('<Leave>', frame_a)    
+framea.pack()
+text.pack()
+Display = Button(framea, height = 2,
+                 width = 20,
+                 text ="Show",
+                 command = lambda:message())
+label.pack()
+Display.pack()
+window.mainloop()
