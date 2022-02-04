e4d90e2d7ab9f0e60895d408cba000d134a174d2
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2022-02-03 21:19:14 -0500

    first comentado

--- src/uimain/principal/first.py
+++ src/uimain/principal/first.py
@@ -34,9 +34,11 @@ class First(Frame):
         self.saludo=Label(frame3,text=greetings,font=('Microsoft Himalaya', 18))
         self.saludo.grid(column=0,row=0, padx=3, pady=3)
 
+        #Se crea el frame donde se presentan fotos relacionados al cine
         frame4=Frame(frame1,width=100,height=130,bg="#FF6A6A")  #El que es rojo
         frame4.grid(column = 0, row= 1, padx=3, pady=3,columnspan=4)
 
+        #Se muestra la primera foto por defecto
         self.label1=Label(frame4)
         self.img1=PhotoImage(file=self.getPath('cinepng.png'),width=400,height=400)
         self.label1['image']=self.img1
@@ -48,8 +50,9 @@ class First(Frame):
         self.img4=PhotoImage(file=self.getPath('cine2.png'),width=400,height=400)
         self.img5=PhotoImage(file=self.getPath('cine4.png'),width=400,height=400)
 
-        self.label1.bind('<ButtonPress-1>', self.cambioAImg2)
+        self.label1.bind('<ButtonPress-1>', self.cambioAImg2)      #para cambiar a la segunda imagen
 
+        #Boton para ir a la ventana principal
         boton=Button(master=frame4,text="Acceder a ventana de usuario",width=25,height=5,command=self.ventanaUsuario)
         boton.grid(column=0,row=1)
 
@@ -63,6 +66,7 @@ class First(Frame):
 
         master['menu']=menubar
 
+        #frame donde van las descripciones
         frameHoja=Frame(frame2,height=50,bg="gray")
         self.hojaIns=Label(frameHoja, text="Pulse sobre cada \n text para cambiar")
         self.hoja1=Label(frameHoja, text="Soy Daniel Daza Macías tengo 20 años\nHabilidades y conocimientos:\nPython: Alto\nRstudio: Básico\nJava: Bueno\nExcel: Básico\nTrabajo en equipo: Bueno \nInglés: Bueno")
@@ -70,7 +74,7 @@ class First(Frame):
         self.hoja3=Label(frameHoja, text="Soy Daniel Santiago Cadavid Montoya  tengo 20 años\nHabilidades y conocimientos:\nPython: Intermedio\nRstudio: Intermedio\nExcel: Intermedio\nTrabajo en equipo: Bueno \nInglés: Bueno\nFrancés: Básico")
         self.hoja4=Label(frameHoja, text="Soy Marlon Calle Areiza tengo 19 años\nHabilidades y conocimientos:\nPython: Bueno\nRstudio: Intermedio\nExcel: Básico\nEnsamblador: Intermedio\nTrabajo en equipo: Bueno \nInglés: Bueno\nFrancés: Bueno")
 
-
+        #Se define un label donde estarán las fotos
         self.hojaFotos1=Fotos(frame2,"1")
         self.hojaFotos2=Fotos(frame2,"2")
         self.hojaFotos3=Fotos(frame2,"3")
@@ -79,6 +83,8 @@ class First(Frame):
         frame2.pack()
         frameHoja.pack()
         self.hojaIns.pack()
+
+        #Se define lo que se hará cuando se haga click sobre cada label
         self.hojaIns.bind('<ButtonPress-1>', self.cambioDeInstrucciones)
         self.hoja1.bind('<ButtonPress-1>', self.cambioAHoja2)
         self.hoja2.bind('<ButtonPress-1>', self.cambioAHoja3)
@@ -135,9 +141,8 @@ class First(Frame):
         self.hoja4.pack()
         self.hojaFotos4.pack()
 
+    #Metodos para los cambios de imagen
     def cambioAImg1(self,action):
-
-
         self.label1.grid_forget()
         self.label1['image']=self.img1
         self.label1.grid(column=0,row=0,padx=3,pady=3)
@@ -171,9 +176,11 @@ class First(Frame):
         self.label1.grid(column=0,row=0,padx=3,pady=3)
         self.label1.bind('<ButtonPress-1>', self.cambioAImg1)
 
+    #Metodo para cambiar de ventana
     def ventanaUsuario(self):
             ventana_usuario=Usuario(self.cine)
 
+    #Metodo para cambiar a la descripcion de la app
     def descripcion(self):
             global greetings    #Descripcion del sistema 
             greetings="En esta aplicación usted podrá registrar un cliente si este es nuevo\nrecomendarle peliculas a clientes viejos o buscar funciones por dia\ntambien podrán seleccionar su asiento y ver cuales estan ocupados.\nUsted también podrá añadir y quitar peliculas, añadir salas\n agregar funciones automatica o manualmente \ny rifar boletos entre los clientes mas fieles"
@@ -181,20 +188,20 @@ class First(Frame):
 
 class Fotos(Frame):
     def __init__(self,a,hoja):
-        super().__init__(a)
+        super().__init__(a)     #Llama al super frame
         self.fot=[]
-        self.hoja1Foto1=[Label(self,width=200, height=200) for i in range(4)]
+        self.hoja1Foto1=[Label(self,width=200, height=200) for i in range(4)]       #Crea 4 labels
         for i in range(1,5):
             fil="hoja"+hoja+"Foto"+str(i)+".png"
-            self.fot.append(PhotoImage(file=self.getPath(fil)))
+            self.fot.append(PhotoImage(file=self.getPath(fil)))     #Se agregan las imagenes a una lista
         i=0
         for r in range(2):
             for c in range(2):
-                self.hoja1Foto1[i]["image"]=self.fot[i]
+                self.hoja1Foto1[i]["image"]=self.fot[i]     #A cada label le agrega una imagen
                 self.hoja1Foto1[i].grid(row=r,column=c)
                 i+=1
     
-    def getPath(self,txt):
+    def getPath(self,txt):      #Para las rutas de las imagenes
         import os
         import sys
         txt= "uimain\\principal\\" +  txt
